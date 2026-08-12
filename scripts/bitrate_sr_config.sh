78ea1a1c9463563ce1ec5e5c795535ae57116ea2
Timofey Glukhikh <tglukhikh@gmail.com>
2026-08-12 21:32:58 +0300

    requirements

--- scripts/bitrate_sr_config.sh
+++ scripts/bitrate_sr_config.sh
@@ -14,6 +14,8 @@ DATA_DIR="${PROJECT_ROOT}/DIV2K/DIV2K_train_HR"
 RUNS_DIR="${PROJECT_ROOT}/runs/bitrate_sr"
 PLOT_SAVE="${RUNS_DIR}/rd.png"
 BPP_CSV="${RUNS_DIR}/bpp_all_sorted.csv"
+# Conda env (path form works when name is not registered in mambaforge)
+CONDA_ENV="/home/timofey.glukhikh/.conda/science_phan"
 # PLOT_SHOW=0
 
 # Image list for training; also written by select_bpp_images.sh
